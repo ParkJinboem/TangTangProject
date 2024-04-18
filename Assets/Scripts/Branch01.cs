4972d50794318a2385214dce00c1ccae5822af08
ParkJinboem <parkjin39@naver.com>
2024-04-18 15:55:03 +0900

    a

--- Assets/Scripts/Branch01.cs
+++ Assets/Scripts/Branch01.cs
@@ -7,7 +7,7 @@ public class Branch01 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Branch01_Main");
     }
 
     // Update is called once per frame
